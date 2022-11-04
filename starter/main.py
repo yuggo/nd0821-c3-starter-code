51e7188c30391a9b0b0cf128a4797f1a28f58ee7
yuggo <denis.vuckovac@gmail.com>
2022-11-04 22:26:25 +0100

    heroku path

--- starter/main.py
+++ starter/main.py
@@ -9,6 +9,22 @@ from starter.ml.model import inference
 import pickle
 import pandas as pd
 
+
+import os
+import subprocess
+
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    dvc_output = subprocess.run(
+        ["dvc", "pull"], capture_output=True, text=True)
+    # print(dvc_output.stdout)
+    # print(dvc_output.stderr)
+    if dvc_output.returncode != 0:
+        print("dvc pull failed")
+    else:
+        os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 app = FastAPI()
 
 
